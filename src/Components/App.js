0690dba3a8fc594340d6c21c53ad67eca17dd874
tarabaz <38695885+tarabaz@users.noreply.github.com>
2019-02-01 15:40:25 +0100

    Upgrade graphic

--- src/Components/App.js
+++ src/Components/App.js
@@ -3,6 +3,8 @@ import React from "react";
 //import { CheckAccount} from "./EosCore";
 
 import Eos from "eosjs";
+import {prezzi} from "./coinmarketcap"
+import axios from 'axios';
 
 
 
@@ -13,6 +15,11 @@ import {
   Grid,
   // TextField,
 } from "@material-ui/core";
+
+
+
+    
+
 
 global.fetch = require('node-fetch')
 const cc = require('cryptocompare')
@@ -57,6 +64,15 @@ const style = {
     padding: "0 0px"
     // boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
   },
+  Paper3: {
+    borderRadius: 10,
+    //marginTop: 15,
+    padding: "10px 10px",
+    textAlign: "center",
+    background: "linear-gradient(45deg, #ffc714 30%, #de4320 90%)",
+   // background: 'linear-gradient(45deg, #420c0c 30%, #800808 90%)',
+
+  }
 }
 
 const config = {
@@ -83,7 +99,8 @@ const PROACCOUNTNAME = "imperareserv";
 let BPACCOUNT;
 let DIVACCOUNT;
 let PROACCOUNT;
-//let ACCOUNT;
+let ACCOUNT;
+let BPLIST;
 //let BPACTIONS;
 //let DIVACTIONS;
 //let PROACTIONS;
@@ -96,15 +113,15 @@ function CheckAccount(account): Promise<any> {
       //console.log(AccInfo)
       if (account === BPACCOUNTNAME) {
         BPACCOUNT=AccInfo;
-        console.log(BPACCOUNT);
+      //  console.log(BPACCOUNT);
       }
       if (account === DIVACCOUNTNAME) {
         DIVACCOUNT=AccInfo;
-                console.log(DIVACCOUNT);
+      //          console.log(DIVACCOUNT);
       }
       if (account === PROACCOUNTNAME) {
         PROACCOUNT=AccInfo;
-        console.log(PROACCOUNT);     
+     //   console.log(PROACCOUNT);     
       }
       if (account === ACCOUNTNAME) {
         ACCOUNT=AccInfo;
@@ -156,7 +173,7 @@ constructor(){
     bpban:"0",
     divban:"0",
     proban:"0",
-    bpUnstake:0,
+    bpUnstake:"0",
     divUnstake:0,
     proUnstake:0,
     totalbpbalance:0,
@@ -167,37 +184,108 @@ constructor(){
     eosPriceBTC: 0,
     eosPricETH: 0,
     tlosEOS:0.03,
+    bpPosition:"-",
+    totalDivids:"",
+    totalTelosDivids:"",
+    bpBlockProduced:"",
+    bpMissedBlock:"",
+    bpMissedBlockRotation:"",
+    bpVotes:0,
+    bpUnpaid:"",
+    totalTelosDivids: 0,
+
+    metadata: [],
   }
-  this.changeState1a = this.changeState1a.bind(this);
-  this.changeState2a = this.changeState2a.bind(this);
-  this.changeState3a = this.changeState3a.bind(this);
+
 
   }
   
   componentDidMount() {
 
 
-    //setTimeout(this.changeState1, 4000);
-    //setTimeout(this.changeState2, 4000);
-    //setTimeout(this.changeState3, 4000);
-
-    //setInterval(this.changeState1a, 2000);
-    //setInterval(this.changeState2a, 2000);
-    //setInterval(this.changeState3a, 2000);
-
-   // CheckAccount(BPACCOUNTNAME).then(setInterval(this.changeState1a, 2000))
-   // CheckAccount(DIVACCOUNTNAME).then(setInterval(this.changeState2a, 2000))
-   // CheckAccount(PROACCOUNTNAME).then(setInterval(this.changeState3a, 2000))
-
-   // CheckAccount(BPACCOUNTNAME).then(setTimeout(this.aspetta, 2000)).then(setTimeout(this.changeState1a, 4000)).then(setTimeout(this.changeState1a, 2000))
-   // CheckAccount(DIVACCOUNTNAME).then(setTimeout(this.aspetta, 2000)).then(setTimeout(this.changeState2a, 4000)).then(setTimeout(this.changeState2a, 2000))
-   // CheckAccount(PROACCOUNTNAME).then(setTimeout(this.aspetta, 2000)).then(setTimeout(this.changeState3a, 4000)).then(setTimeout(this.changeState3a, 2000))
-
-    CheckAccount(BPACCOUNTNAME)
-    CheckAccount(DIVACCOUNTNAME)
-    CheckAccount(PROACCOUNTNAME)
     CheckAccount(ACCOUNTNAME)
+////////////////////////////////////////////////////////////////////////////U
 
+    eos.getAccount(BPACCOUNTNAME).then(AccInfo => {
+        BPACCOUNT = AccInfo;
+      let valore1 = 0
+      BPACCOUNT.refund_request ?
+        valore1 = this.balance(BPACCOUNT.refund_request.net_amount) + this.balance(BPACCOUNT.refund_request.cpu_amount)
+        // console.log("valore c'è") 
+        : valore1 = 0
+
+      this.setState({
+        bpUnstake: valore1,
+        bpbalance: BPACCOUNT.core_liquid_balance,
+        bpcpu: BPACCOUNT.total_resources.cpu_weight,
+        bpban: BPACCOUNT.total_resources.net_weight,
+      });
+      let total = this.balance(this.state.bpbalance) + this.balance(this.state.bpcpu) + this.balance(this.state.bpban) + this.balance(this.state.bpUnstake)
+      this.setState({
+        totalbpbalance: total,
+
+      })
+    })
+      .catch(error => {
+        console.error(error);
+      })
+
+    eos.getAccount(DIVACCOUNTNAME).then(AccInfo => {
+
+      
+    
+        DIVACCOUNT = AccInfo;
+        //          console.log(DIVACCOUNT);
+      let valore = 0
+      DIVACCOUNT.refund_request ? valore = this.balance(DIVACCOUNT.refund_request.net_amount) + this.balance(DIVACCOUNT.refund_request.cpu_amount) : valore = 0
+
+      this.setState({
+        divUnstake: valore,
+
+        divbalance: DIVACCOUNT.core_liquid_balance,
+        divcpu: DIVACCOUNT.total_resources.cpu_weight,
+        divban: DIVACCOUNT.total_resources.net_weight,
+        totaldivbalance: this.balance(this.state.divbalance) + this.balance(this.state.divcpu) + this.balance(this.state.divban) + this.balance(this.state.divUnstake),
+        // divUnstake: this.balance(DIVACCOUNT.refund_request.net_amount) + this.balance(DIVACCOUNT.refund_request.cpu_amount),
+
+      });
+      let total = this.balance(this.state.divbalance) + this.balance(this.state.divcpu) + this.balance(this.state.divban) + this.balance(this.state.divUnstake)
+      this.setState({
+        totaldivbalance: total,
+
+      })
+    })
+      .catch(error => {
+        console.error(error);
+      });
+
+    eos.getAccount(PROACCOUNTNAME).then(AccInfo => {
+      //console.log(AccInfo)
+
+        PROACCOUNT = AccInfo;
+      let valore = 0
+      PROACCOUNT.refund_request ? valore = this.balance(PROACCOUNT.refund_request.net_amount) + this.balance(PROACCOUNT.refund_request.cpu_amount) : valore = 0
+
+      this.setState({
+        proUnstake: valore,
+        probalance: PROACCOUNT.core_liquid_balance,
+        procpu: PROACCOUNT.total_resources.cpu_weight,
+        proban: PROACCOUNT.total_resources.net_weight,
+        // proUnstake: this.balance(PROACCOUNT.refund_request.net_amount) + this.balance(PROACCOUNT.refund_request.cpu_amount),
+      });
+      let total = this.balance(this.state.probalance) + this.balance(this.state.procpu) + this.balance(this.state.proban) + this.balance(this.state.proUnstake)
+      this.setState({
+        totalprobalance: total,
+
+      })
+
+    })
+      .catch(error => {
+        console.error(error);
+      });
+   
+
+//////////////////////////////////////////////////////////////////////////////
     
  //   fetch("https://api.chainrift.com/v1/Public/Market?")
  //     .then(results => {
@@ -214,139 +302,67 @@ constructor(){
           eosPriceETH: prices.ETH,
           eosPriceEUR: prices.EUR
         })
-        // -> { USD: 1100.24, EUR: 1039.63 }
+        // -> { USD: 1100.24, EUR: 1039.63 }s
        // console.log(this.state.eosPriceEUR)
        // console.log(this.state.eosPriceUSD)
       })
       .catch(console.error)
 
 
-  
+  /*
+    axios.get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=6d18f57f-0ba0-40e6-8c93-3686ea23fa56')
+      .then(res => {
+        console.log(res.data)
+        const persons = res.data
+        this.setState = ({
+          metadata: persons,
+        })
+      })
 
+
+*/
+
+    eos.getProducers({ json: true, limit: 500 }).then(lista => {
+      //console.log(lista);
+      BPLIST = lista;
+      console.log(BPLIST);
+      
+
+      for (var i =0; i < 40; i++) {
+        let bp = BPLIST.rows[i].owner
+        if (bp === "tlosimperabp") {
+          console.log(bp)
+          console.log(BPLIST.rows[i]);
+          let vot = parseFloat(BPLIST.rows[i].total_votes)
+              this.setState({
+            bpPosition:i+1,
+                bpBlockProduced: BPLIST.rows[i].lifetime_produced_blocks ,
+                bpMissedBlock: BPLIST.rows[i].lifetime_missed_blocks ,
+                bpMissedBlockRotation: BPLIST.rows[i].missed_blocks_per_rotation ,
+                bpVotes: vot.toFixed(0) ,
+                bpUnpaid: BPLIST.rows[i].unpaid_blocks,
+          })
+}
+
+}
+
+    })
     };
 
 aspetta = () => {
 console.log("aspettando 2 sec")
 }
 
-  onClick(event) {
-    console.log("1")
-    console.log("2")
-
-    this.changeState1a()
-
-    this.changeState2a()
-    console.log("3")
-
-    this.changeState3a()
-
-  }
-
-caricaTutto = () => {
-  let valore1 = 0
-  BPACCOUNT.refund_request ?
-    valore1 = this.balance(BPACCOUNT.refund_request.net_amount) + this.balance(BPACCOUNT.refund_request.cpu_amount)
-    // console.log("valore c'è") 
-    : valore1 = 0
-
-  this.setState({
-    bpUnstake: valore1,
-    bpbalance: BPACCOUNT.core_liquid_balance,
-    bpcpu: BPACCOUNT.total_resources.cpu_weight,
-    bpban: BPACCOUNT.total_resources.net_weight,
-    totalbpbalance: this.balance(this.state.bpbalance) + this.balance(this.state.bpcpu) + this.balance(this.state.bpban) + this.balance(this.state.bpUnstake),
-    // bpUnstake: this.balance(BPACCOUNT.refund_request.net_amount) + this.balance(BPACCOUNT.refund_request.cpu_amount),
-
-  });
-
-  let valore2 = 0
-  DIVACCOUNT.refund_request ? valore2 = this.balance(DIVACCOUNT.refund_request.net_amount) + this.balance(DIVACCOUNT.refund_request.cpu_amount) : valore2 = 0
-
-  this.setState({
-    divUnstake: valore2,
-
-    divbalance: DIVACCOUNT.core_liquid_balance,
-    divcpu: DIVACCOUNT.total_resources.cpu_weight,
-    divban: DIVACCOUNT.total_resources.net_weight,
-    totaldivbalance: this.balance(this.state.divbalance) + this.balance(this.state.divcpu) + this.balance(this.state.divban) + this.balance(this.state.divUnstake),
-    // divUnstake: this.balance(DIVACCOUNT.refund_request.net_amount) + this.balance(DIVACCOUNT.refund_request.cpu_amount),
-
-  });
-    //console
-
-  let valore3 = 0
-  PROACCOUNT.refund_request ? valore3 = this.balance(PROACCOUNT.refund_request.net_amount) + this.balance(PROACCOUNT.refund_request.cpu_amount) : valore3 = 0
-
-  this.setState({
-    proUnstake: valore3,
-    probalance: PROACCOUNT.core_liquid_balance,
-    procpu: PROACCOUNT.total_resources.cpu_weight,
-    proban: PROACCOUNT.total_resources.net_weight,
-    totalprobalance: this.balance(this.state.probalance) + this.balance(this.state.procpu) + this.balance(this.state.proban) + this.balance(this.state.proUnstake),
-    // proUnstake: this.balance(PROACCOUNT.refund_request.net_amount) + this.balance(PROACCOUNT.refund_request.cpu_amount),
-  });
-
-}
-
-  changeState1a = ()=> {
-    let valore = 0
-     BPACCOUNT.refund_request ? 
-    valore = this.balance(BPACCOUNT.refund_request.net_amount) + this.balance(BPACCOUNT.refund_request.cpu_amount)
-   // console.log("valore c'è") 
-    : valore = 0 
-   
-     this.setState({
-      bpUnstake: valore,
-      bpbalance: BPACCOUNT.core_liquid_balance,
-      bpcpu: BPACCOUNT.total_resources.cpu_weight,
-      bpban: BPACCOUNT.total_resources.net_weight,
-      totalbpbalance: this.balance(this.state.bpbalance) + this.balance(this.state.bpcpu) + this.balance(this.state.bpban) + this.balance(this.state.bpUnstake),
-     // bpUnstake: this.balance(BPACCOUNT.refund_request.net_amount) + this.balance(BPACCOUNT.refund_request.cpu_amount),
-
-    });
-  }
+ 
 
   balance = (value) => {
     return parseFloat(value);
   }
   
-  changeState2a = () => {
-    let valore = 0
-     DIVACCOUNT.refund_request ? valore = this.balance(DIVACCOUNT.refund_request.net_amount) + this.balance(DIVACCOUNT.refund_request.cpu_amount) : valore = 0 
-
-    this.setState({
-      divUnstake: valore,
-
-      divbalance: DIVACCOUNT.core_liquid_balance,
-      divcpu: DIVACCOUNT.total_resources.cpu_weight,
-      divban: DIVACCOUNT.total_resources.net_weight,
-      totaldivbalance: this.balance(this.state.divbalance) + this.balance(this.state.divcpu) + this.balance(this.state.divban) + this.balance(this.state.divUnstake),
-     // divUnstake: this.balance(DIVACCOUNT.refund_request.net_amount) + this.balance(DIVACCOUNT.refund_request.cpu_amount),
-
-    });
-    //console.log(this.state.totaldivbalance)
-
-  }
-
-  changeState3a = () => {
-    let valore = 0
-     PROACCOUNT.refund_request ? valore = this.balance(PROACCOUNT.refund_request.net_amount) + this.balance(PROACCOUNT.refund_request.cpu_amount) : valore=0
-
-    this.setState({
-      proUnstake: valore,
-      probalance: PROACCOUNT.core_liquid_balance,
-      procpu: PROACCOUNT.total_resources.cpu_weight,
-      proban: PROACCOUNT.total_resources.net_weight,
-      totalprobalance: this.balance(this.state.probalance) + this.balance(this.state.procpu) + this.balance(this.state.proban) + this.balance(this.state.proUnstake),
-     // proUnstake: this.balance(PROACCOUNT.refund_request.net_amount) + this.balance(PROACCOUNT.refund_request.cpu_amount),
-    });
-
  
 
 
-  }
-
- 
+  
   render() {
     return(
     <div>
@@ -373,28 +389,7 @@ caricaTutto = () => {
 
 
         </div>
-        <div align="center">
-        <Button
-          variant="contained"
-          color="primary"
-          align="center"
-          size="small"
-            fullWidth
-          style={
-            {
-              // ...style.button,
-              position: "relative",
-              textAlign: "center",
-              marginBottom:10,
-              //width:"100%"
-            }
-          }
-          onClick={this.caricaTutto}
-          elevation={5}
-        >LOAD/REFRESH < br/>
-        2 click min
 
-        </Button></div>
         <Grid container spacing={24} direction="row">
           <Grid item xs={6} sm={6}>
 
@@ -571,6 +566,82 @@ caricaTutto = () => {
         </Grid>
 
         <Grid container spacing={24} direction="row">
+          <Grid item xs={12} sm={6}>
+
+            <Paper
+              style={{
+                ...style.Paper3,
+                topMargin: 10,
+                //  height: 320
+              }}
+              elevation={10}
+            >
+
+              <Typography component="p" align="center" style={{
+                color: "black",
+                topMargin: -10,
+              }}>
+                Lifetime Produced Block: <strong> {this.state.bpBlockProduced} </strong><br />
+                Missed Block per Rotation: <strong> {this.state.bpMissedBlockRotation } </strong><br />
+                Lifetime Missed Block: <strong> {this.state.bpMissedBlock} </strong><br />
+                Votes: <strong> {this.state.bpVotes} </strong><br />
+                Position: <strong> {this.state.bpPosition}° </strong><br />
+                Unpaid Block: <strong> {this.state.bpUnpaid } </strong><br />
+
+              </Typography>
+            </Paper>
+          </Grid>
+
+          <Grid item xs={12} sm={6}>
+
+            <Paper
+              style={{
+                ...style.Paper3,
+                topMargin: 10,
+                //  height: 320
+              }}
+              elevation={10}
+            >
+              <Typography variant="headline" variant="">
+                <strong>Dividendi</strong> 
+              </Typography>
+              <Typography variant="headline" variant="caption">
+                I dividendi sono calcolati sul totale diviso 7
+              </Typography>
+              <hr style={{ height: 1}}/>
+              <Typography variant="headline" variant="caption">
+                Dividendi del wallet IMPERADIVIDS
+              </Typography>
+              <Typography component="p" align="center" style={{
+                color: "black",
+                topMargin: -10,
+              }}>
+              TELOS: <strong> {(this.state.totaldivbalance/7).toFixed(4)} </strong><br />
+               </Typography>
+              <Typography variant="headline" variant="caption">
+                USD: {((this.state.totaldivbalance / 7).toFixed(4) * this.state.tlosEOS * this.state.eosPriceUSD).toFixed(2)}$  - EUR: {((this.state.totaldivbalance / 7).toFixed(4) * this.state.tlosEOS * this.state.eosPriceEUR).toFixed(2)}€
+              </Typography>
+              <hr style={{ height: 1 }} />
+              <Typography variant="headline" variant="caption">
+                Dividendi su calcolo Totale esclusa la riserva per progetti
+              </Typography>
+              <Typography component="p" align="center" style={{
+                color: "black",
+                topMargin: -10,
+              }}>
+                TELOS: <strong> {((this.state.totaldivbalance + this.state.totalbpbalance )/ 7).toFixed(4)} </strong><br />
+              </Typography>
+              <Typography variant="headline" variant="caption">
+                USD: {(((this.state.totaldivbalance + this.state.totalbpbalance) / 7).toFixed(4) * this.state.tlosEOS * this.state.eosPriceUSD).toFixed(2)}$  - EUR: {(((this.state.totaldivbalance + this.state.totalbpbalance) / 7).toFixed(4) * this.state.tlosEOS * this.state.eosPriceEUR).toFixed(2)}€
+              </Typography>
+            </Paper>
+          </Grid>
+
+
+
+        </Grid>
+
+        <Grid container spacing={24} direction="row">
           <Grid item xs={12} sm={12}>
 
             <Paper
@@ -581,7 +652,12 @@ caricaTutto = () => {
               }}
               elevation={10}
             >
-
+              <Typography variant="headline" variant="caption" style={{
+                color: "white",
+                topMargin: -10,
+              }}>
+                Capitalizzazione Totale
+              </Typography>
               <Typography component="p" align="left" style={{
                 color: "white",
                 topMargin: -10,
@@ -600,6 +676,7 @@ caricaTutto = () => {
 
 
         </Grid>
+        
       </div>
 
     )}
